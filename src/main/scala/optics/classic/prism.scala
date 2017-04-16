d147f730e858cc87b60295c1914e70f754ca3b3f
lemastero <adres.mistrza@gmail.com>
2017-04-16 15:37:12 +0200

    implement Prism

--- src/main/scala/optics/classic/prism.scala
+++ src/main/scala/optics/classic/prism.scala
@@ -7,9 +7,17 @@ abstract class Prism[S, T] extends APrism[S, T] { self =>
 
   def reverseGet(t: T): S
 
-  final def modify(f: T => T): S => S = ???
+  final def modify(f: T => T): S => S =
+    s => getOption(s)
+      .map(f)
+      .map(reverseGet)
+      .getOrElse(s)
 
-  final def composePrism[C](other: Prism[T, C]): Prism[S, C] = ???
+  final def composePrism[C](other: Prism[T, C]): Prism[S, C] = new Prism[S, C] {
+    override def getOption(s: S): Option[C] = self.getOption(s).flatMap(other.getOption)
+
+    override def reverseGet(c: C): S = self.reverseGet(other.reverseGet(c))
+  }
 }
 
 object Prism {
